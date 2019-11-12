c339cdb7077493317839fed8d5bd5ca3e2d9ff54
User <User@M580V-01>
2019-11-12 19:11:35 +0800

    評論已可以修改 新增 顯示全部 (未套版)

--- MeetTogether/src/main/java/com/meetogether/eeit10913/controller/ProductController.java
+++ MeetTogether/src/main/java/com/meetogether/eeit10913/controller/ProductController.java
@@ -1,28 +1,20 @@
 package com.meetogether.eeit10913.controller;
 
-import java.io.File;
-import java.sql.Blob;
 import java.util.List;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.sql.rowset.serial.SerialBlob;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.meetogether.eeit10901.model.MemberBean;
-import com.meetogether.eeit10901.service.MemberService;
+import com.meetogether.eeit10908.model.ActBean;
 import com.meetogether.eeit10913.model.ReviewBean;
 import com.meetogether.eeit10913.service.ProductService;
-import com.meetogether.eeit10927.model.Member;
 
 @Controller
 public class ProductController {
@@ -39,8 +31,14 @@ public class ProductController {
 	public void setService(ProductService service) {
 		this.service = service;
 	}
+//	@RequestMapping(value = "/getOneReview")
+//	public String getOneReview(@RequestParam(value="reviewId") Integer reviewId,Model model) {
+//		ReviewBean rb = service.getReviewById(reviewId);
+//		model.addAttribute("product",rb);
+//		return "/review";
+//	}
 	
-
+//新增方法
 	@RequestMapping(value = "/addProduct", method = RequestMethod.GET)
 	public String getaddReview(Model model) {
 		ReviewBean rb = new ReviewBean();
@@ -49,18 +47,26 @@ public class ProductController {
 	}
 
 	@RequestMapping(value = "/addProduct", method = RequestMethod.POST)
-	public String abc(@ModelAttribute("reviewBean")ReviewBean rb) {
+	public String abc(@ModelAttribute("reviewBean") ReviewBean rb) {
 		System.out.println(rb.getEventComment());
 		System.out.println(rb.getEventStars());
 		service.add(rb);
 		return "redirect:/allreview";
 	}
-//	@RequestMapping(value = "/review", method = RequestMethod.POST)
-//	public String processAddNewReview(@ModelAttribute("reviewBean") ReviewBean rb) {
-//		service.add(rb);
-//		return "index";
-//	}
-
+//修改方法
+	@RequestMapping(value = "/editProduct", method = RequestMethod.GET)
+	public String getActByIdForm(@RequestParam("reviewId") Integer reviewId,Model model) {
+		System.out.println("reviewId => " + reviewId);
+		model.addAttribute("editProduct",service.getReviewById(reviewId));
+	    return "eeit10913/editProduct";
+	}
+	
+	@RequestMapping(value = "/editProduct", method = RequestMethod.POST)
+	public String update(@ModelAttribute("editProduct") ReviewBean aa) {
+		service.update(aa);
+		return "redirect:/allreview";
+	}
+//查詢所有
 	@RequestMapping("/allreview")
 	public String list(Model model) {
 		List<ReviewBean> list = service.selectALL();
